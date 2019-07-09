cc13b86b7577d719290e2c5bb9343c7c5778ab88
luchunyu <liu@static-172-108-0-85.dlls.tx.frontiernet.net>
2019-07-09 14:44:34 +0800

    add

---
+++ src/test/java/com/bingkun/activity/ActivityServiceApplicationTests.java
@@ -0,0 +1,22 @@
+package com.bingkun.activity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivityServiceApplicationTests {
+
+    @Autowired
+    private Jedis jedis;
+
+    @Test
+    public void contextLoads() {
+        jedis.get("hello");
+    }
+
+}
